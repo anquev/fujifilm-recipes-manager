0b5a8012a3ee101f32befa9317a8a4a4774a41a4
anquev <114365240+anquev@users.noreply.github.com>
2026-04-11 16:01:56 +0200

    add directory auto-suggestion for imports

--- src/data/models.py
+++ src/data/models.py
@@ -156,10 +156,7 @@ class FujifilmExif(models.Model):
 
     @classmethod
     def get_or_create(cls, **fields) -> "FujifilmExif":
-        try:
-            obj, _ = cls.objects.get_or_create(**fields)  # type: ignore[attr-defined]
-        except cls.MultipleObjectsReturned:
-            obj = cls.objects.filter(**fields).first()
+        obj, _ = cls.objects.get_or_create(**fields)  # type: ignore[attr-defined]
         return obj
 
     # Properties
